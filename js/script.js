5af358f1583dd846dd6b672528e82aa8538a32b3
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-08-05 13:39:54 +0000

    Added global variables at script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+    // Set Up Global Variables
+    let targetSequence = [];
+    let targetSequenceCopy = [];
+    let playerSequence = [];
+    let audioPlayer;
+    let soundPlaying;
+    let stepCount = 0;
+    let strictMode = false;
+    let isGameOn = false;
+    let isGameRunning = false;
+    let flashInterval;
+    let flashCounter = 0;
+    let colors = ["green","blue","yellow","red"];
+    let colorSounds = {green:document.getElementById('sound-green'),
+        blue:document.getElementById('sound-blue'),
+        yellow:document.getElementById('sound-yellow'),
+        red:document.getElementById('sound-red')};
+
     
 });
